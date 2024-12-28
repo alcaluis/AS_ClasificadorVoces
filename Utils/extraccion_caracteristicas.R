b4bcc705b59b968837bd8dd8f60cd74de0bd884b
jopebrui <jopebrui@alumni.uv.es>
2024-12-28 10:57:07 +0100

    Implementación LPCC y STE

--- Utils/extraccion_caracteristicas.R
+++ Utils/extraccion_caracteristicas.R
@@ -17,3 +17,46 @@ STE <- function(wave, wlen, ovlp = 0) {
   # NOTA: Devuelve normalizado
   return(cbind(time.frame, STE/max(STE)))
 }
+
+
+
+
+# LPCC
+LPCC <- function(wave, wlen, order, ovlp = 0) {
+  
+  N <- length(wave@left)          
+  step <- wlen - (ovlp * wlen / 100) 
+  indices <- seq(1, N - wlen, by = step) 
+  m <- length(indices)           
+  
+  LPCC_features <- list()        
+  
+  for (i in 1:m) {
+    # Seleccionar ventana de audio
+    start_idx <- indices[i]
+    end_idx <- min(start_idx + wlen - 1, N)
+    frame <- wave@left[start_idx:end_idx]
+    
+    # Calcular los LPC
+    lpc_coeffs <- lpc(frame, order = order)$a  # Coeficientes LPC
+    
+    # Transformar LPC a LPCC
+    lpcc <- numeric(order)
+    for (n in 1:order) {
+      if (n <= length(lpc_coeffs)) {
+        lpcc[n] <- lpc_coeffs[n]
+      }
+      if (n > length(lpc_coeffs)) {
+        lpcc[n] <- 0
+        for (k in 1:(n - 1)) {
+          lpcc[n] <- lpcc[n] + (k / n) * lpcc[k] * lpc_coeffs[n - k]
+        }
+      }
+    }
+    
+    LPCC_features[[i]] <- lpcc  # Guardar LPCC de esta ventana
+  }
+  
+  return(LPCC_features)
+}
+

c0d5fdb4af2b5da2b867eb8f44d57110e6cf68d0
jugarbus <jugarbus@alumni.uv.es>
2025-01-02 00:52:55 +0100

    Commit 6

--- Utils/extraccion_caracteristicas.R
+++ Utils/extraccion_caracteristicas.R
@@ -22,23 +22,32 @@ STE <- function(wave, wlen, ovlp = 0) {
 
 # PGM 
 
-PGM <- function(signal, sampling_rate, window_size=100 , overlap=0) {
-  # Duración de la señal en tiempo
-  longitud_senal <- length(signal) / sampling_rate
-  # Número de ventanas
-  num_ventanas <- round(longitud_senal / window_size)
+PGM <- function(signal) {
   
-  # Dividir la señal en ventanas
-  windows <- sliding.window(signal, wl = window_size, ovlp = overlap)
+  melfcc <- melfcc(signal,
+                  minfreq = 50,
+                  maxfreq = 4000,
+                  sr = signal@samp.rate, 
+                  hoptime = 0.01,
+                  wintime = 0.025, usecmp = TRUE) 
   
-  # Calcular las características gaussianas para cada ventana
-  features <- apply(windows, 2, function(window) {
-    mean_val <- mean(window)
-    std_val <- sd(window)
-    c(mean_val, std_val)
-  })
+  coefs <- gsignal::idct(melfcc)
   
-  return(t(features))
+  medias_filas <- apply(coefs, 1, mean)
+  covarianzas <- cov(t(coefs))
+  diagonal <- diag(covarianzas)
+  
+  c1 <- (medias_filas - min(medias_filas)) / (max(medias_filas)- min(medias_filas))
+  c2 <- (diagonal - min(diagonal)) / (max(diagonal)- min(diagonal))
+
+  # Crear un data frame con medias y diagonas de varianzas
+  meanvar_carac <- data.frame(
+    medias = c1,
+    varianzas = c2)
+  
+  return(meanvar_carac)
+  
+  
 }
 
 
@@ -46,14 +55,12 @@ PGM <- function(signal, sampling_rate, window_size=100 , overlap=0) {
 # PS
 
 PS <- function(signal, sampling_rate, span=NULL) {
-  # Duración de la señal en tiempo
-  longitud_senal <- length(signal) / sampling_rate
-  
+
   # Calcular el espectro de potencia usando la función spectrum
   if (is.null(span)) {
-    spec <- spectrum(signal, method = "pgram", log = "dB", plot = FALSE)
+    spec <- spectrum(signal, method = "pgram", log = "no", plot = FALSE)
   } else {
-    spec <- spectrum(signal, method = "pgram", log = "dB", plot = FALSE, spans = span)
+    spec <- spectrum(signal, method = "pgram", log = "no", plot = FALSE, spans = span)
   }  
   
   # Crear un data frame con las frecuencias y la potencia
@@ -65,3 +72,43 @@ PS <- function(signal, sampling_rate, span=NULL) {
   return(espectro_potencia)
 }
 
+
+
+
+
+
+
+# POWER SPECTRUM ??
+
+PS_2 <- function(signal, sampling_rate) {
+
+    n <- length(signal)
+    fs <- sampling_rate
+    
+    # Calcular la FFT (Transformada Rápida de Fourier)
+    fft_result <- fft(signal)
+    
+    # Calcular el espectro de potencia (módulo al cuadrado de la FFT)
+  
+    
+    xk <- (1/(n^2)) * abs(fft_result)^2
+    
+    # Calcular el espectro de potencia de una cara
+    xk <- xk[1:(n/2+1)]
+    
+    # Duplicar los valores (excepto DC y Nyquist) para el espectro de una cara
+    xk[2:(length(xk)-1)] <- 2 * xk[2:(length(xk)-1)]
+    
+    
+    # Calcular las frecuencias
+    frequencies <- (0:(n-1)) * (fs / n)
+    
+  
+  # Crear un data frame con las frecuencias y la potencia
+  espectro_potencia <- data.frame(
+    Frecuencia = frequencies,
+    Potencia = xk
+  )
+  
+  return(espectro_potencia)
+}

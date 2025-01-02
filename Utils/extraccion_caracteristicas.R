ec8bb030b8f60ebce2001a8e4db5bf4ed36a3fea
jugarbus <jugarbus@alumni.uv.es>
2025-01-02 14:43:52 +0100

    Commit 7

--- Utils/extraccion_caracteristicas.R
+++ Utils/extraccion_caracteristicas.R
@@ -1,25 +1,3 @@
-# STE
-STE <- function(wave, wlen, ovlp = 0) {
-  tm <- 1 / wave@samp.rate
-  N <- length(wave@left)
-  t <- seq(0, (N-1)*tm, length.out=N)
-  n <- length(wave)
-  step10 <- seq(1, n-wlen, wlen - (ovlp * wlen/100))
-  m10 <- length(step10)
-  
-  STE <- numeric(m10)
-  time.frame <- numeric(m10)
-  for (i in 1:m10) {
-    frame.wave <- wave[step10[i]:(step10[i]+wlen)]
-    STE[i] <- mean(frame.wave@left * frame.wave@left)
-    time.frame[i] <- mean(t[step10[i]: (step10[i] + wlen)])
-  }
-  # NOTA: Devuelve normalizado
-  return(cbind(time.frame, STE/max(STE)))
-}
-
-
-
 # PGM 
 
 PGM <- function(signal) {
@@ -75,40 +53,3 @@ PS <- function(signal, sampling_rate, span=NULL) {
 
 
 
-
-
-
-# POWER SPECTRUM ??
-
-PS_2 <- function(signal, sampling_rate) {
-
-    n <- length(signal)
-    fs <- sampling_rate
-    
-    # Calcular la FFT (Transformada Rápida de Fourier)
-    fft_result <- fft(signal)
-    
-    # Calcular el espectro de potencia (módulo al cuadrado de la FFT)
-  
-    
-    xk <- (1/(n^2)) * abs(fft_result)^2
-    
-    # Calcular el espectro de potencia de una cara
-    xk <- xk[1:(n/2+1)]
-    
-    # Duplicar los valores (excepto DC y Nyquist) para el espectro de una cara
-    xk[2:(length(xk)-1)] <- 2 * xk[2:(length(xk)-1)]
-    
-    
-    # Calcular las frecuencias
-    frequencies <- (0:(n-1)) * (fs / n)
-    
-  
-  # Crear un data frame con las frecuencias y la potencia
-  espectro_potencia <- data.frame(
-    Frecuencia = frequencies,
-    Potencia = xk
-  )
-  
-  return(espectro_potencia)
-}
